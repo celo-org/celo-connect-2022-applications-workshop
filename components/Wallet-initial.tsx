1e3a75e264cd3e4a576a0ff7f9bc6eaf477c8f0b
Camila Rondinini <crondinini@gmail.com>
2022-03-25 15:24:12 +0100

    rename final Wallet file to be default one for now

---
+++ components/Wallet-initial.tsx
@@ -0,0 +1,25 @@
+import styles from "./Header.module.css";
+
+export default function Wallet() {
+  const address = false;
+  const network = { name:'placeholder'};
+  const destroy = () => {};
+  const connect = () => {};
+
+  return (
+    <div className="container">
+      {address ? (
+        <>
+          <div className={styles.summary}>
+            <code>{`Network: ${network.name} | Address: ${address}`}</code>
+          </div>
+          <button onClick={destroy}>Disconnect wallet</button>
+        </>
+      ) : (
+        <>
+          <button onClick={connect}>Connect wallet</button>
+        </>
+      )}
+    </div>
+  );
+}
