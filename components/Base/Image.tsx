d9584b1ded09815a817a0a296a8aebeb100ce4c4
Camila Rondinini <crondinini@gmail.com>
2022-03-29 17:10:17 +0200

    styling: image default background plus fix

--- components/Base/Image.tsx
+++ components/Base/Image.tsx
@@ -5,10 +5,10 @@ import PlaceholderSvg from "./PlaceholderSvg"
 export default function Img({ url } : { url: string}) {
   return (
     <div className={stylesCard.imageContainer}>
-        {url === "" ?
-          <PlaceholderSvg />
-         : <img src={url} alt="Artwork for bid" width={400} height={400}/>
-        }
-      </div>
+      {!url ?
+        <PlaceholderSvg />
+        : <img src={url} alt="Artwork for auction" width={400} height={400}/>
+      }
+    </div>
   )
 };
